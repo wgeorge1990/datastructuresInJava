a3de2d209ff305c6ffb38a31d3aa59063a663c69
wgeorge1990 <williameverettgeorge@icloud.com>
2020-05-24 20:13:53 -0400

    refactored isBalancedExpression()

--- src/com/dataStructures/BalancedExpressions.java
+++ src/com/dataStructures/BalancedExpressions.java
@@ -1,8 +1,40 @@
 package com.dataStructures;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 public class BalancedExpressions {
+
+    private static List<Character> leftBrackets;
+    private static List<Character> rightBrackets;
+
+    //REFACTORED SOLUTION
+    public static boolean newIsBalancedExpression(){
+
+        return false;
+    }
+
+    private static boolean isLeftBracket(char c) {
+        //If you create this way you cant pass args to construcor
+//        ArrayList<Character> leftBrackets = new ArrayList();
+
+        leftBrackets = Arrays.asList('(', '<', '[', '{');
+        return leftBrackets.contains(c);
+    }
+
+    private static boolean isRightBracket(char c) {
+        rightBrackets = Arrays.asList(')', '>', ']', '}');
+        return rightBrackets.contains(c);
+    }
+
+    private static boolean bracketsMatch(char left, char right) {
+      return    (right == ')' && left != '(') ||
+                (right == '>' && left != '<') ||
+                (right == ']' && left != '[') ||
+                (right == '}' && left != '{');
+    }
 
     //CLASSIC SOLUTION BASED ON MOST ALGORITHM BOOKS
     public static boolean isBalancedExpression(String expression){
@@ -10,21 +42,14 @@ public class BalancedExpressions {
         //iterate string and for each char
         // if (openingBracket) or (closedBracket) push onto stack
         // if anything else ignore
+
         Stack<Character> brackets = new Stack<>();
         for (char c: expression.toCharArray()){
-            if(c == '(' || c == '<' || c == '[' || c =='{' ){
-                brackets.push(c);
-            }
-
-            if(c == ')' || c == '>' || c == ']' || c =='}' ){
+            if(isLeftBracket(c)){ brackets.push(c); }
+            if(isRightBracket(c)){
                 if(brackets.empty()) return false;
                 var top = brackets.pop();
-                if (
-                        (c == ')' && top != '(') ||
-                        (c == '>' && top != '<') ||
-                        (c == ']' && top != '[') ||
-                        (c == '}' && top != '{')
-                ) return false;
+                if (bracketsMatch(top, c)) return false;
             }
         }
         return brackets.empty();
