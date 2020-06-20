967189dc4382ba44e9017ad180e9f3b6c6708408
wgeorge1990 <williameverettgeorge@icloud.com>
2020-06-20 00:39:26 -0400

    reverse queue using a stack.

--- src/com/dataStructures/Main.java
+++ src/com/dataStructures/Main.java
@@ -1,20 +1,33 @@
 package com.dataStructures;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Queue;
 
 public class Main {
 
   public static void main(String[] args) {
-    Stack stack = new Stack();
-    stack.bPush(10);
-    stack.bPush(20);
-    stack.bPush(30);
-    System.out.println(stack);
+    Queue<Integer> q = new ArrayDeque<>();
+    q.add(1);
+    q.add(2);
+    q.add(3);
+    q.add(4);
+    System.out.println(q);
+    reverse(q);
+  }
 
-    stack.push(1);
-    stack.push(2);
-    stack.push(3);
-    stack.print();
+  public static void reverse(Queue<Integer> q){
+    //  add // remove // isEmpty
+    //[ 10, 20, 30 ]
+    java.util.Stack<Integer> stack = new java.util.Stack<Integer>();
+
+    while (!q.isEmpty()){
+      stack.push(q.remove());
+    }
+
+    while (!stack.isEmpty())
+      q.add(stack.pop());
+    System.out.println(q.toString());
   }
 
 }
